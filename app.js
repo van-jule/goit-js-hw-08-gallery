3ca00640b950924a6d3c53e983c2d36e5e8db019
Yuliia Kharchenko <jkharchenko2008@gmail.com>
2021-07-23 18:28:35 +0300

    Выполняет ДЗ 8

--- app.js
+++ app.js
@@ -45,3 +45,94 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ]
+
+const galleryListEl = document.querySelector('.js-gallery')
+const closeBtnEl = document.querySelector('.lightbox__button')
+const modal = document.querySelector('.js-lightbox')
+const modalImageEl = document.querySelector('.lightbox__image')
+const overlayEL = document.querySelector('.lightbox__overlay')
+// const galleryItemsEl = document.querySelectorAll('.gallery__item')
+
+const imagesMarkup = createGalleryMarkup(galleryItems)
+galleryListEl.insertAdjacentHTML('beforeend', imagesMarkup)
+
+galleryListEl.addEventListener('click', onOpenModal)
+closeBtnEl.addEventListener('click', onCloseModal)
+overlayEL.addEventListener('click', onOverlayClickCloseModal)
+window.addEventListener('keydown', onCloseModalESC)
+window.addEventListener('keydown', onScrollGalleryRight)
+
+function createGalleryMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    })
+    .join('')
+}
+
+// const galleryItemEl = document.querySelector('.gallery__item')
+// const galleryImageEl = document.querySelector('.gallery__image')
+
+function onOverlayClickCloseModal(evt) {
+  console.log('клик по оверлей')
+  console.log('currentTarget', evt.currentTarget)
+  console.log('target', evt.target)
+
+  // и без єтого работает, но для практики:
+  if (evt.target === evt.currentTarget) {
+    console.log('кликнули именно в оверлей!!!')
+    onCloseModal()
+  }
+}
+
+function onCloseModalESC(evt) {
+  if (evt.key !== 'Escape') {
+    return
+  }
+  console.log('закрываю модалку по', evt.key)
+  onCloseModal()
+}
+
+function onScrollGalleryRight(evt) {
+  if (evt.key !== 'ArrowRight') {
+    return
+  }
+  console.log(evt.key)
+  console.log(evt.target)
+  // console.log(galleryItemsEl)
+}
+
+function onOpenModal(evt) {
+  if (!evt.target.classList.contains('gallery__image')) {
+    return
+  }
+
+  modal.classList.add('is-open')
+  evt.preventDefault()
+
+  modalImageEl.src = evt.target.dataset.source
+  modalImageEl.alt = evt.target.alt
+  console.log('modalImageEl.src after opening:', modalImageEl.src)
+  console.log('modalImageEl.alt after opening:', modalImageEl.alt)
+}
+
+function onCloseModal(evt) {
+  modal.classList.remove('is-open')
+  modalImageEl.src = ''
+  modalImageEl.alt = ''
+  console.log('modalImageEl.src after closing:', modalImageEl.src)
+  console.log('modalImageEl.alt after closing:', modalImageEl.alt)
+}
